7f83218cc9028389f76dbc6ad72c67a22e26a929
Mikhail Lazarev <mikhail@mlabs.city>
2023-02-08 20:37:46 +0400

    added: documentation examples

--- cardano-sdk-playground/cardano-embedded-sdk/src/api.rs
+++ cardano-sdk-playground/cardano-embedded-sdk/src/api.rs
@@ -1,3 +1,15 @@
+//! Embedano SDK API
+//! SDK API functions aim to be as easy to use as possible.
+//! Main ingredients required:
+//! - entropy (seed) for HD wallet, that usually stored in the memory of the embedded device
+//! - password provided by the user
+//! - derivation path of required keys (as all operations using keys)
+//!
+//! Functions of API are built on top of the more low-level functions
+//! that can be found in `types.rs`.
+//!
+//! Examples can be found in docs for each function.
+
 use crate::{
     bip::bip39::Entropy,
     crypto::Ed25519Signature,
@@ -8,14 +20,129 @@ use derivation_path::{ChildIndex, DerivationPath};
 
 const EXTERNAL_CHAIN_CODE: u32 = 0;
 
-#[derive(Clone)]
-pub enum KeyType {
-    AccountKey { account_gap: u32 },
-    AddressKey { account_gap: u32, address_gap: u32 },
+/// Derive extended private key from entropy (seed) for specified derivation path.
+/// # Example
+/// ```
+/// use cardano_embedded_sdk::bip::bip39::{dictionary, Entropy, Mnemonics};
+/// use cardano_embedded_sdk::types::XPrvKey;
+/// use cardano_embedded_sdk::api as embedano;
+/// use derivation_path::{DerivationPath};
+///
+/// let mnemonics = Mnemonics::from_string(&dictionary::ENGLISH,"all all all all all all all all all all all all",).unwrap();
+/// let entropy = Entropy::from_mnemonics(&mnemonics).unwrap();
+/// let password = b"embedano";
+/// let path: DerivationPath = "m/1852'/1815'/0'/0/0".parse().unwrap();
+///
+/// let private_key: XPrvKey  = embedano::derive_key(&entropy, password, &path);
+/// ```
+pub fn derive_key(entropy: &Entropy, password: &[u8], path: &DerivationPath) -> XPrvKey {
+    let mut key = XPrvKey::from_entropy(entropy, password);
+    for index in path.into_iter().map(adjust_hardened) {
+        key = key.derive(index);
+    }
+    key
 }
 
-// proof public key ownership (account or address level)
-pub fn proof_ownership(
+/// Extension of `derive_key` that also returns public key.
+/// # Example
+/// ```
+/// use cardano_embedded_sdk::bip::bip39::{dictionary, Entropy, Mnemonics};
+/// use derivation_path::{ChildIndex, DerivationPath};
+/// use cardano_embedded_sdk::api as embedano;
+///
+///
+/// let mnemonics = Mnemonics::from_string(&dictionary::ENGLISH,"all all all all all all all all all all all all",).unwrap();
+/// let entropy = Entropy::from_mnemonics(&mnemonics).unwrap();
+/// let password = b"embedano";
+/// let path: DerivationPath = "m/1852'/1815'/0'/0/0".parse().unwrap();
+/// let (pub_key, prv_key) = embedano::derive_key_pair(&entropy, password, &path);
+/// ```
+pub fn derive_key_pair(
+    entropy: &Entropy,
+    password: &[u8],
+    path: &DerivationPath,
+) -> (XPrvKey, XPubKey) {
+    let private = derive_key(entropy, password, path);
+    let public = private.to_public();
+    (private, public)
+}
+
+/// Sign transaction id with private key derived for provided path.
+/// Transaction id (`TxId`) - is hash of transaction body.
+/// # Example
+/// ```
+/// use cardano_embedded_sdk::bip::bip39::{dictionary, Entropy, Mnemonics};
+/// use cardano_embedded_sdk::types::{TxId, XPrvKey};
+/// use cardano_embedded_sdk::api as embedano;
+/// use derivation_path::{DerivationPath};
+///
+/// let mnemonics = Mnemonics::from_string(&dictionary::ENGLISH,"all all all all all all all all all all all all",).unwrap();
+/// let entropy = Entropy::from_mnemonics(&mnemonics).unwrap();
+/// let password = b"embedano";
+/// let path: DerivationPath = "m/1852'/1815'/0'/0/0".parse().unwrap();
+///
+/// let tx_id = TxId::from_hex("bb1eb401cd03b0cd8caa08997df0a2ab226772c4d3a08adfb5a60ba34de12dfb").unwrap();
+/// let signature = embedano::sign_tx_id(&tx_id, &entropy, password, &path);
+/// ```
+pub fn sign_tx_id(
+    tx_id: &TxId,
+    entropy: &Entropy,
+    password: &[u8],
+    path: &DerivationPath,
+) -> Ed25519Signature {
+    sign_data(tx_id.to_bytes(), entropy, password, path)
+}
+
+/// Sign binary data with private key derived for provided path.
+/// # Example
+/// ```
+/// use cardano_embedded_sdk::bip::bip39::{dictionary, Entropy, Mnemonics};
+/// use cardano_embedded_sdk::types::{TxId, XPrvKey};
+/// use cardano_embedded_sdk::api as embedano;
+/// use derivation_path::{DerivationPath};
+///
+/// let mnemonics = Mnemonics::from_string(&dictionary::ENGLISH,"all all all all all all all all all all all all",).unwrap();
+/// let entropy = Entropy::from_mnemonics(&mnemonics).unwrap();
+/// let password = b"embedano";
+/// let path: DerivationPath = "m/1852'/1815'/0'/0/0".parse().unwrap();
+///
+/// let signature = embedano::sign_data(b"some bytes", &entropy, password, &path);
+/// ```
+pub fn sign_data(
+    data: &[u8],
+    entropy: &Entropy,
+    password: &[u8],
+    path: &DerivationPath,
+) -> Ed25519Signature {
+    derive_key(entropy, password, path).sign(data)
+}
+
+/// Prove public key ownership (account or address level) by signing nonce. See also `KeyType` docs.
+///
+/// If function returns `Some(signature)`, caller should be able to verify signature
+/// with public key provided as function argument.
+/// To control how deep to search for the corresponding private key `KeyType`
+/// parameter should be specified.
+/// # Example
+/// ```
+/// use cardano_embedded_sdk::bip::bip39::{dictionary, Entropy, Mnemonics};
+/// use cardano_embedded_sdk::types::{TxId, XPrvKey};
+/// use cardano_embedded_sdk::api as embedano;
+/// use derivation_path::{DerivationPath};
+///
+/// let mnemonics = "aim wool into nose tell ball arm expand design push elevator multiply glove lonely minimum";
+/// let mnemonics = Mnemonics::from_string(&dictionary::ENGLISH, mnemonics).unwrap();
+/// let entropy = Entropy::from_mnemonics(&mnemonics).unwrap();
+/// let password = b"embedano";
+/// let nonce = "some nonce".as_bytes();
+/// let path: DerivationPath = "m/1852'/1815'/0'/0/0".parse().unwrap();
+///
+/// let (_, pub_key) = embedano::derive_key_pair(&entropy, password, &path);
+/// let key_type = embedano::KeyType::AddressKey { account_gap: 5, address_gap: 5};
+/// let proof = embedano::prove_ownership(&nonce, &pub_key, &entropy, password,key_type).unwrap();
+/// assert!(&pub_key.verify(nonce, &proof))
+/// ```
+pub fn prove_ownership(
     nonce: &[u8],
     payment_key: &XPubKey,
     entropy: &Entropy,
@@ -26,7 +153,7 @@ pub fn proof_ownership(
     find_matching_private_key(payment_key, &root_key, key_type).map(|key| key.sign(nonce))
 }
 
-// helper function for `proof_ownership`
+// Helper function for `prove_ownership`
 fn find_matching_private_key(
     pub_key: &XPubKey,
     root_key: &XPrvKey,
@@ -65,44 +192,25 @@ fn find_matching_private_key(
     }
 }
 
-// sign TxId (body hash)
-pub fn sign_tx_id(
-    tx_id: &TxId,
-    entropy: &Entropy,
-    password: &[u8],
-    path: &DerivationPath,
-) -> Ed25519Signature {
-    sign_data(tx_id.to_bytes(), entropy, password, path)
+/// Allows to specify gaps for account and address indexes for `prove_ownership` function.
+///
+/// `prove_ownership` uses algorithm similar to account discovery (BIP-0044) to find
+/// corresponding private key and sign nonce to prove key ownership.
+/// `KeyType` allows to limit number of account and address indexes to iterate
+/// through while deriving private key.
+/// Usually account discovery uses indexes from 0 to 20 (inclusive).
+#[derive(Clone)]
+pub enum KeyType {
+    /// Use together with `prove_ownership` to limit the number of account indexes to search.
+    AccountKey { account_gap: u32 },
+    /// Use together with `prove_ownership` to limit the number of account and address
+    /// indexes to search.
+    AddressKey { account_gap: u32, address_gap: u32 },
 }
 
-// sign arbitrary binary data
-pub fn sign_data(
-    data: &[u8],
-    entropy: &Entropy,
-    password: &[u8],
-    path: &DerivationPath,
-) -> Ed25519Signature {
-    derive_key(entropy, password, path).sign(data)
-}
-
-// get private key for derivation path from seed and password
-pub fn derive_key(entropy: &Entropy, password: &[u8], path: &DerivationPath) -> XPrvKey {
-    let mut key = XPrvKey::from_entropy(entropy, password);
-    for index in path.into_iter().map(adjust_hardened) {
-        key = key.derive(index);
-    }
-    key
-}
-
-// get private and corresponding public key for derivation path from seed and password
-pub fn derive_key_pair(
-    entropy: &Entropy,
-    password: &[u8],
-    path: &DerivationPath,
-) -> (XPrvKey, XPubKey) {
-    let private = derive_key(entropy, password, path);
-    let public = private.to_public();
-    (private, public)
+/// Harden derivation index.
+pub fn harden(i: u32) -> u32 {
+    i + 0x80000000
 }
 
 fn adjust_hardened(index: &ChildIndex) -> u32 {
@@ -110,10 +218,6 @@ fn adjust_hardened(index: &ChildIndex) -> u32 {
         ChildIndex::Hardened(i) => harden(i),
         ChildIndex::Normal(i) => i,
     }
-}
-
-pub fn harden(i: u32) -> u32 {
-    i + 0x80000000
 }
 
 // todo: more tests
@@ -135,7 +239,7 @@ mod tests {
         nonce: &[u8],
         key_type: KeyType,
     ) -> Option<bool> {
-        proof_ownership(nonce, pub_key, entropy, password, key_type)
+        prove_ownership(nonce, pub_key, entropy, password, key_type)
             .map(|s| pub_key.verify(nonce, &s))
     }
 
